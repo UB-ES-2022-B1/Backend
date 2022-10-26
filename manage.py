12c972688741f490792700e0faf795994249ee21
Endimbeer Enrique Nuñez Matos <72166134+endix24@users.noreply.github.com>
2022-10-27 00:40:38 +0200

    Update manage.py

--- manage.py
+++ manage.py
@@ -15,7 +15,7 @@ def main():
         load_dotenv('./.env')
 
     # When running on Azure App Service you should use the production settings.
-    settings_module = "Backend.production" if 'WEBSITE_HOSTNAME' in os.environ else 'Backend.settings'
+    settings_module = "Backend.Production" if 'WEBSITE_HOSTNAME' in os.environ else 'Backend.settings'
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
     try:
