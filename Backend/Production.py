0c025137f6944e60f66997a5786f58a215da2c6f
Endimbeer Enrique Nuñez Matos <72166134+endix24@users.noreply.github.com>
2022-11-18 22:27:39 +0100

    Update Production.py

--- Backend/Production.py
+++ Backend/Production.py
@@ -6,6 +6,7 @@ import os
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 DEBUG = False
+CORS_ORIGIN_ALLOW_ALL = True
 
 # WhiteNoise configuration
 MIDDLEWARE = [
