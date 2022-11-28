82e7da429389f0be43bcaf27d33081de6d658a01
Endimbeer Enrique Nuñez Matos <enunezma82@alumnes.ub.edu>
2022-11-28 18:25:04 +0100

    Corrección de errores de acceso a la base de datos.

--- Backend/settings.py
+++ Backend/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'Backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db_2.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
