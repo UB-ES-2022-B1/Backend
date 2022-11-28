5060adbcef69b063b3fd45e62b16a330dd873573
Endimbeer Enrique Nuñez Matos <72166134+endix24@users.noreply.github.com>
2022-11-28 23:14:46 +0100

    Update views.py

--- houses/views.py
+++ houses/views.py
@@ -10,7 +10,6 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
 import datetime
-import uuid
 from io import BytesIO
 import os
 from .models import House
