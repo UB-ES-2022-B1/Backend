571883789dbbd54d17db5f42f0c92d0932e3def3
Endimbeer Enrique Nuñez Matos <enunezma82@alumnes.ub.edu>
2022-12-07 20:09:01 +0100

    Corrección de errores de acceso a la base de datos.

--- Backend/settings.py
+++ Backend/settings.py
@@ -84,7 +84,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'Backend.wsgi.application'
-hostname = os.environ['DBHOST']
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
@@ -93,13 +92,8 @@ hostname = os.environ['DBHOST']
 # username (not @sever-name).
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ['DBNAME'],
-        'HOST': hostname,
-        'USER': os.environ['DBUSER'],
-        'PASSWORD': os.environ['DBPASS'],
-        'PORT': '5432',
-        'OPTIONS': {'sslmode': 'require'}
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
