cb7f67dbf1579f3b3a9c0caeea0061409de21aa9
Endimbeer Enrique Nuñez Matos <enunezma82@alumnes.ub.edu>
2022-12-08 22:06:34 +0100

    Corrección de errores de acceso a la base de datos.

--- Backend/settings.py
+++ Backend/settings.py
@@ -91,10 +91,20 @@ WSGI_APPLICATION = 'Backend.wsgi.application'
 
 # Configure Postgres database; the full username for PostgreSQL flexible server is
 # username (not @sever-name).
+# DBHOST is only the server name, not the full URL
+hostname = os.environ['DBHOST']
+
+# Configure Postgres database; the full username for PostgreSQL flexible server is
+# username (not @sever-name).
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['DBNAME'],
+        'HOST': hostname ,
+        'USER': os.environ['DBUSER'],
+        'PASSWORD': os.environ['DBPASS'],
+        'PORT': '5432',
+        'OPTIONS': {'sslmode': 'require'}
     }
 }
 
