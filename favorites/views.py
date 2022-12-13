3f46f711e6d1f9b933b7e87c20a99b22612727e3
Endimbeer Enrique Nuñez Matos <enunezma82@alumnes.ub.edu>
2022-12-13 18:57:18 +0100

    Adición de condiciones para añadir y eliminar favoritos.

--- favorites/views.py
+++ favorites/views.py
@@ -22,19 +22,23 @@ class AddToFavorites(APIView):
             house = House.objects.get(id=request.data["id_house"])
             client = Client.objects.get(email=request.user.email)
 
-            # Campos a almacenar en la base de datos.
-            fields = dict()
-            fields["id_client"] = client.pk
-            fields["id_house"] = house.pk
-            fields["timestamp"] = datetime.date.today()
-
-            serializer = FavoritesSerializer(data=fields)
-            if serializer.is_valid():
-                serializer.save()
-                return Response({'success': True, 'msg': 'Added Success'},
+            if request.data["toAdd"] == True:
+                # Campos a almacenar en la base de datos.
+                fields = dict()
+                fields["id_client"] = client.pk
+                fields["id_house"] = house.pk
+                fields["timestamp"] = datetime.date.today()
+                serializer = FavoritesSerializer(data=fields)
+                if serializer.is_valid():
+                    serializer.save()
+                    return Response({'success': True, 'msg': 'Added to favorites'},
+                                    status=status.HTTP_201_CREATED)
+                return Response({'success': False, 'msg': 'Error with database!'},
+                                status=status.HTTP_400_BAD_REQUEST)
+            else:
+                Favorites.objects.filter(id_client=client.pk, id_house=house.pk).delete()
+                return Response({'success': True, 'msg': 'Removed from favorites'},
                                 status=status.HTTP_201_CREATED)
-            return Response({'success': False, 'msg': 'Error with database!'},
-                            status=status.HTTP_400_BAD_REQUEST)
         except ObjectDoesNotExist:
             return Response({'success': False, 'msg': 'Incorrect house id!'},
                             status=status.HTTP_400_BAD_REQUEST)
